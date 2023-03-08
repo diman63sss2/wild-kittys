70adb1d6ed7eb8220b3c028d30a719c9ae3ed40b
Miloslava <106954260+millyWelly@users.noreply.github.com>
2023-03-09 01:46:23 +0400

    Начало верстки страницы Корзина (доделать галочки и работу кнопки)

--- src/pages/Brush.jsx
+++ src/pages/Brush.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import card from '../img/catalog-cart-stub.png';
+import select from "../img/icon_select_1.svg";
+import "../styles/App.css";
 
 const Brush = () => {
 
@@ -8,8 +11,34 @@ const Brush = () => {
                 <div className="brush__content">
                     <h1>Корзина</h1>
                     <h2>товары в корзине</h2>
-                    <div>
-
+                    <div className="brush__useful_content">
+                        <div className="brush__list">
+                            <div className="brush__list__item">
+                                <img className="brush__list__item__pic" src={card}/>
+                                <div className="brush__list__item__text_block">
+                                    <h3 className="brush__list__item__text_block__name">Название</h3>
+                                    <h4 className="brush__list__item__text_block__price">9999.99</h4>
+                                </div>
+                                <img className="brush__list__item__select" src={select}/>
+                            </div>
+                            <div className="brush__list__item">
+                                <img className="brush__list__item__pic" src={card}/>
+                                <div className="brush__list__item__text_block">
+                                    <h3 className="brush__list__item__text_block__name">Название</h3>
+                                    <h4 className="brush__list__item__text_block__price">9999.99</h4>
+                                </div>
+                                <img className="brush__list__item__select" src={select}/>
+                            </div>
+                            <div className="brush__list__item">
+                                <img className="brush__list__item__pic" src={card}/>
+                                <div className="brush__list__item__text_block">
+                                    <h3 className="brush__list__item__text_block__name">Название</h3>
+                                    <h4 className="brush__list__item__text_block__price">9999.99</h4>
+                                </div>
+                                <img className="brush__list__item__select" src={select}/>
+                            </div>
+                        </div>
+                        <button className="brush__btn">Выставить на аукцион</button>
                     </div>
                 </div>
             </div>
