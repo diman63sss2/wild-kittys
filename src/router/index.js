796303b6348dc93f6c73bdbe7a255f3e209db1c0
diman <diman63sss2@mail.ru>
2023-03-09 15:53:18 +0400

    Miloslava changes

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Page404 from "../pages/Page404";
 import Profile from "../pages/Profile";
 import {Navigate} from "react-router-dom";
 import React from "react";
+import Brush from "../pages/Brush";
 
 
 export const privateRoutes = [
@@ -14,6 +15,7 @@ export const privateRoutes = [
     {path: '/support', component: <Support/>, exact: true},
     {path: '/messages', component: <Messages/>, exact: true},
     {path: '/profile', component: <Profile/>, exact: true},
+    {path: '/brush', component: <Brush/>, exact: true},
     {path: '/error-page', component: <Page404/>, exact: true},
     {path: '*', component: <Navigate to="/error-page" />, exact: true},
 ]
@@ -22,5 +24,6 @@ export const publicRoutes = [
     {path: '', component: <Main/>, exact: true},
     {path: '/catalog', component: <Catalog/>, exact: true},
     {path: '/support', component: <Support/>, exact: true},
+    {path: '/brush', component: <Brush/>, exact: true},
     {path: '/error-page', component: <Page404/>, exact: true},
 ]
